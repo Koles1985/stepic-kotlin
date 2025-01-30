a6441acd6ac643eda0ccbb361a97a4b535025076
Koles1985 <orehov.1985@bk.ru>
2025-01-30 14:49:45 +0300

    Stepic 2.2.7

--- src/stepic/kotlin/cycles_andbranches_2_2_6/Task.kt
+++ src/stepic/kotlin/cycles_andbranches_2_2_6/Task.kt
@@ -18,5 +18,19 @@ fun main(args: Array<String>){
         return count
     }
 
-    println(calculateEvenDigit("1234567890"))
+   // println(calculateEvenDigit("1234567890"))
+
+   val words = arrayOf("KLPQYM", "BUG", "KHQR", "KLPQYM", "JVOJTD", "JVOJTD", "BUG", "BUG", "KHQR", "BUG",
+       "KLPQYM", "JVOJTD", "KLPQYM", "JVOJTD", "QLSPA", "KHQR", "JVOJTD", "KLPQYM", "JVOJTD", "KLPQYM",
+       "KLPQYM", "JVOJTD", "JVOJTD", "BUG", "KLPQYM", "KHQR", "JVOJTD", "KHQR", "KLPQYM", "KLPQYM", "JVOJTD",
+       "KLPQYM", "BUG", "BUG", "BUG")
+
+    var i: Int = 0
+    for(word in words){
+        if(word.equals("BUG")){
+            i++
+        }
+    }
+
+    println(i)
 }
